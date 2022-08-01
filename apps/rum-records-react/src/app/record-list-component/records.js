f20a5559fe8a6543c86d80aa6e82fa2a1019a1fd
Carly Richmond <carly.richmond@elastic.co>
2022-08-01 16:54:40 +0100

    Adding records data and images

--- apps/rum-records-react/src/app/record-list-component/records.js
+++ apps/rum-records-react/src/app/record-list-component/records.js
@@ -1,20 +1,33 @@
-const records = [{
-    title: 'Hot Fuss',
-    artist: 'The Killers',
-    formats: ['Vinyl', 'CD', 'Digital Download'],
-    imagePath: '../../assets/images/records/killers-the-hot-fuss.png'
+const records = [
+{
+    title: 'Abbey Road',
+    artist: 'The Beetles',
+    formats: ['CD', 'LP', 'Digital Download'],
+    imagePath: '../../assets/images/records/the-beatles-abbey-road.avif'
 },
 {
-    title: 'Sam\'s Town',
-    artist: 'The Killers',
+    title: 'Sgt Pepper\'s Lonely Hearts Club Band',
+    artist: 'The Beetles',
+    formats: ['CD', 'LP'],
+    imagePath: '../../assets/images/records/the-beatles-sgt-peppers-lonely-hearts-club-band.avif'
+},
+{
+    title: 'True Blue',
+    artist: 'Madonna',
     formats: ['CD', 'Digital Download'],
-    imagePath: '../../assets/images/records/killers-sams-town.jpg'
+    imagePath: '../../assets/images/records/madonna-true-blue.avif'
 },
 {
-    title: 'Future Nostalgia',
-    artist: 'Dua Lipa',
-    formats: ['Vinyl', 'CD'],
-    imagePath: '../../assets/images/records/dua-lipa-future-nostalgia.png'
+    title: 'Born in the USA',
+    artist: 'Bruce Springsteen',
+    formats: ['CD', 'LP', 'Digital Download'],
+    imagePath: '../../assets/images/records/bruce-spingsteen-born-in-the-usa.avif'
+},
+{
+    title: 'Grace Jones',
+    artist: 'Island Life',
+    formats: ['CD', 'LP'],
+    imagePath: '../../assets/images/records/grace-jones-island-life.avif'
 },
 {
     title: 'The Dark Side of the Moon',
@@ -23,22 +36,88 @@ const records = [{
     imagePath: '../../assets/images/records/pink-floyd-dark-side-of-the-moon.webp'
 },
 {
+    title: 'Unknown Pleasures',
+    artist: 'Jo Division',
+    formats: ['CD', 'LP', 'Digital Download'],
+    imagePath: '../../assets/images/records/joy-division-unknown-pleasures.avif'
+},
+{
     title: 'Rumours',
     artist: 'Fleetwood Mac',
     formats: ['CD', 'LP'],
-    imagePath: '../../assets/images/records/fleetwood-mac-rumours.png'
+    imagePath: '../../assets/images/records/fleetwood-mac-rumours.avif'
 },
 {
     title: 'Nevermind',
     artist: 'Nirvana',
     formats: ['CD', 'Digital Download'],
-    imagePath: '../../assets/images/records/nirvana-nevermind.png'
+    imagePath: '../../assets/images/records/nirvana-nevermind.avif'
+},
+{
+    title: 'Ready to Die',
+    artist: 'The Notorious BIG',
+    formats: ['CD', 'Digital Download'],
+    imagePath: '../../assets/images/records/the-notorious-big-ready-to-die.avif'
 },
 {
     title: 'Aladdin Sane',
     artist: 'David Bowie',
+    formats: ['CD', 'LP'],
+    imagePath: '../../assets/images/records/david-bowie-aladdin-sane.avif'
+},
+{
+    title: 'The Dreaming',
+    artist: 'Kate Bush',
     formats: ['CD', 'LP', 'Digital Download'],
-    imagePath: '../../assets/images/records/david-bowie-aladdin-sane.webp'
+    imagePath: '../../assets/images/records/kate-bush-the-dreaming.avif'
+},
+{
+    title: 'Definitely Maybe',
+    artist: 'Oasis',
+    formats: ['CD', 'LP', 'Digital Download'],
+    imagePath: '../../assets/images/records/oasis-definitely-maybe.avif'
+},
+{
+    title: 'Sticky Fingers',
+    artist: 'The Rolling Stones',
+    formats: ['LP', 'Digital Download'],
+    imagePath: '../../assets/images/records/the-rolling-stones-sticky-fingers.avif'
+},
+{
+    title: 'Parallel Lines',
+    artist: 'Blondie',
+    formats: ['CD', 'Digital Download'],
+    imagePath: '../../assets/images/records/blondie-parallel-lines.avif'
+},
+{
+    title: 'The Freewheelin\' Bob Dylan',
+    artist: 'Bob Dylan',
+    formats: ['CD', 'LP', 'Digital Download'],
+    imagePath: '../../assets/images/records/bob-dylan-the-freewheelin-bob-dylan.avif'
+},
+{
+    title: 'Led Zeppelin',
+    artist: 'Led Zeppelin',
+    formats: ['CD', 'LP'],
+    imagePath: '../../assets/images/records/led-zeppelin-led-zeppelin.avif'
+},
+{
+    title: 'London Calling',
+    artist: 'The Clash',
+    formats: ['CD', 'LP'],
+    imagePath: '../../assets/images/records/the-clash-london-calling.avif'
+},
+{
+    title: 'Straight Outta Compton',
+    artist: 'NWA',
+    formats: ['CD', 'Digital Download'],
+    imagePath: '../../assets/images/records/nwa-straight-outta-compton.avif'
+},
+{
+    title: 'The Ramones',
+    artist: 'The Ramones',
+    formats: ['CD', 'LP'],
+    imagePath: '../../assets/images/records/the-ramones-the-ramones.avif'
 }
 ];
 
