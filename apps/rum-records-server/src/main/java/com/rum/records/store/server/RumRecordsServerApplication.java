be8e407839a95f266fbaa3cb7a4deaa92be2772b
Carly Richmond <carly.richmond@elastic.co>
2022-08-05 12:02:29 +0100

    Adding Elastic Agent for Java

--- apps/rum-records-server/src/main/java/com/rum/records/store/server/RumRecordsServerApplication.java
+++ apps/rum-records-server/src/main/java/com/rum/records/store/server/RumRecordsServerApplication.java
@@ -3,10 +3,13 @@ package com.rum.records.store.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
+
 @SpringBootApplication
 public class RumRecordsServerApplication {
 
 	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(RumRecordsServerApplication.class, args);
 	}
 
