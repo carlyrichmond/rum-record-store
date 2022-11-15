704e90b9dddfdc6c76b115f371c0b0dc7a4f748e
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2022-11-15 16:00:29 +0000

    Changing urls

--- apps/rum-records-react/src/environments/environment.prod.js
+++ apps/rum-records-react/src/environments/environment.prod.js
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3333',
+  baseUrl: 'https://6373b65a54536a25c7fb079c--record-store-server-node-js.netlify.app',
   elastic_deployment_url: 'https://e27b61b76ab2406a9da20520f384f88b.apm.eu-west-2.aws.cloud.es.io:443'
 };
