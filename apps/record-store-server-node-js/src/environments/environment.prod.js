b22713ed083c61e471b5fcd97cc3ae4bd8d86654
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2022-11-16 11:14:25 +0000

    Removing netlify references

--- apps/record-store-server-node-js/src/environments/environment.prod.js
+++ apps/record-store-server-node-js/src/environments/environment.prod.js
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   defaultPort: 3333,
-  allowedOrigin: 'https://6373b65a54536a25c7fb079c--record-store-server-node-js.netlify.app'
+  allowedOrigin: 'http://localhost:4200'
 };
