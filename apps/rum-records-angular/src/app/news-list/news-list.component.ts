1993cdbb8344641ba936515449d663aec1e0cc79
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-06-08 14:16:01 +0100

    Adding error to example

--- apps/rum-records-angular/src/app/news-list/news-list.component.ts
+++ apps/rum-records-angular/src/app/news-list/news-list.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NewsListComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    throw new Error("No news available");
+  }
 }
