6422e9baee7fb2ccd252079270239582bb0ca843
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2023-10-09 15:57:27 +0100

    Fixing broken tests and updating code snippet image

--- apps/rum-records-react/src/app/main-splash-component/main-splash-component.spec.js
+++ apps/rum-records-react/src/app/main-splash-component/main-splash-component.spec.js
@@ -1,8 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+
 import MainSplashComponent from './main-splash-component';
 describe('MainSplashComponent', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<MainSplashComponent />);
+    const { baseElement } = render(<BrowserRouter><MainSplashComponent /></BrowserRouter>);
     expect(baseElement).toBeTruthy();
   });
 
--- apps/rum-records-react/src/app/search-bar-component/search-bar-component.spec.js
+++ apps/rum-records-react/src/app/search-bar-component/search-bar-component.spec.js
@@ -1,13 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+
 import SearchBarComponent from './search-bar-component';
+
 describe('SearchBarComponent', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<SearchBarComponent />);
+    const { baseElement } = render(<BrowserRouter><SearchBarComponent /></BrowserRouter>);
     expect(baseElement).toBeTruthy();
   });
 
   it('should contain search input', () => {
-    const { getByPlaceholderText } = render(<SearchBarComponent />);
+    const { getByPlaceholderText } = render(<BrowserRouter><SearchBarComponent /></BrowserRouter>);
     expect(getByPlaceholderText(/Browse our record selection today!/gi)).toBeTruthy();
   });
 });
