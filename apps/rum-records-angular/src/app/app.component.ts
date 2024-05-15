19fe1ea1d72b56789a06024f7415222e2f794c78
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2024-05-15 21:58:38 +0100

    Adding RUM agent to Angular application

--- apps/rum-records-angular/src/app/app.component.ts
+++ apps/rum-records-angular/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ErrorHandler } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ApmErrorHandler, ApmModule, ApmService } from '@elastic/apm-rum-angular';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMugSaucer, faRecordVinyl } from '@fortawesome/free-solid-svg-icons';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'rum-record-store-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule],
+  imports: [ApmModule, RouterOutlet, FontAwesomeModule],
+  providers: [ApmService, {
+      provide: ErrorHandler,
+      useClass: ApmErrorHandler
+    }],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
@@ -15,4 +22,16 @@ export class AppComponent {
   title = 'rum-records-angular';
   faMugSaucer = faMugSaucer;
   faRecordVinyl = faRecordVinyl;
+
+  constructor(apmService: ApmService) {
+    apmService.init({
+      serviceName: 'rum-records-angular-ui',
+      distributedTracingOrigins: ['http://localhost:8080'], // front to back instrumentation
+      serverUrl: environment.elastic_deployment_url,
+      serviceVersion: '1',
+      environment: 'dev',
+      // How to enable debug mode if needed
+      //logLevel: 'debug'
+    });
+  }
 }
